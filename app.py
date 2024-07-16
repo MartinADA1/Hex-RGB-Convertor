a2052c4dccc7748de76b4b362704c6a84cdd8bd2
Martin-59A <martin.nkama@59a.co.uk>
2024-07-16 13:08:06 +0100

    main conversion function complete, handles requests (flask app)

--- app.py
+++ app.py
@@ -40,3 +40,52 @@ def hsl_to_hex(h, s, l):
 def hsl_to_rgba(h, s, l, a=1.0):
     r, g, b = hsl_to_rgb(h, s, l)
     return rgba_to_hex(r, g, b, a)
+
+@app.route('/convert', methods=['GET'])
+def convert():
+    input_value = request.args.get('value')
+    conversion_type = request.args.get('type')
+
+    try:
+        if conversion_type == 'hex_to_rgb':
+            rgb = hex_to_rgb(input_value)
+            return jsonify({'rgb': rgb})
+        elif conversion_type == 'rgb_to_hex':
+            r, g, b = map(int, input_value.split(','))
+            hex_value = rgb_to_hex(r, g, b)
+            return jsonify({'hex': hex_value})
+        elif conversion_type == 'hex_to_rgba':
+            rgba = hex_to_rgba(input_value)
+            return jsonify({'rgba': rgba})
+        elif conversion_type == 'rgba_to_hex':
+            r, g, b, a = map(float, input_value.split(','))
+            hex_value = rgba_to_hex(r, g, b, a)
+            return jsonify({'hex': hex_value})
+        elif conversion_type == 'rgb_to_hsl':
+            r, g, b = map(int, input_value.split(','))
+            hsl = rgb_to_hsl(r, g, b)
+            return jsonify({'hsl': hsl})
+        elif conversion_type == 'hsl_to_rgb':
+            h, s, l = map(int, input_value.split(','))
+            rgb = hsl_to_rgb(h, s, l)
+            return jsonify({'rgb': rgb})
+        elif conversion_type == 'rgba_to_rgb':
+            r, g, b, a = map(int, input_value.split(','))
+            rgb = rgba_to_rgb(r, g, b, a)
+            return jsonify({'rgb': rgb})
+        elif conversion_type == 'rgb_to_rgba':
+            r, g, b = map(int, input_value.split(','))
+            rgba = rgb_to_rgba(r, g, b)
+            return jsonify({'rgba': rgba})
+        elif conversion_type == 'hsl_to_hex':
+            h, s, l = map(int, input_value.split(','))
+            hex_value = hsl_to_hex(h, s, l)
+            return jsonify({'hex': hex_value})
+        elif conversion_type == 'hsl_to_rgba':
+            h, s, l = map(int, input_value.split(','))
+            rgba = hsl_to_rgba(h, s, l)
+            return jsonify({'rgba': rgba})
+        else:
+            return jsonify({'error': 'Invalid conversion type'}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
